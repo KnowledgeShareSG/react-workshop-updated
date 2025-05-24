ac3a97fe83f10f9b20e5d4c130ba60634f5471b3
Renji <renji129@gmail.com>
2025-05-24 20:56:15 +0800

    Add table component from shadcn

--- src/views/watchlist/Watchlist.tsx
+++ src/views/watchlist/Watchlist.tsx
@@ -1,10 +1,19 @@
 import {Button} from "@/components/ui/button.tsx";
 import {SquarePen} from "lucide-react";
 import {StockSearch} from "@/views/stock-search/StockSearch.tsx";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 export const Watchlist = () => {
     return (
-        <div className="w-full px-5">
+        <div className="w-full px-5 mt-15">
             <div className="flex flex-wrap justify-between items-center gap-y-4">
                 <h1 className="w-full text-center sm:w-auto sm:text-left text-2xl font-bold text-gray-800">
                     Watchlist
@@ -22,8 +31,29 @@ export const Watchlist = () => {
                     </Button>
                 </div>
             </div>
-            <div className="mt-8 bg-white px-0 py-6">
-                <p className="text-gray-500 text-center py-8">No stocks in your watchlist yet. Use the search above to add stocks.</p>
+            <div className="mt-5 bg-white px-0 py-6 overflow-x-auto">
+                <Table>
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="text-left md:table-cell">Invoice</TableHead>
+                            <TableHead className="text-left hidden md:table-cell">Status</TableHead>
+                            <TableHead className="text-left md:table-cell">Method</TableHead>
+                            <TableHead className="text-left md:table-cell">Date</TableHead>
+                            <TableHead className="text-left hidden md:table-cell">Amount</TableHead>
+                        </TableRow>
+                    </TableHeader>
+
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="text-left md:table-cell">INV001</TableCell>
+                            <TableCell className="text-left hidden md:table-cell">Paid</TableCell>
+                            <TableCell className="text-left md:table-cell">Credit Card</TableCell>
+                            <TableCell className="text-left md:table-cell">May 21, 2025</TableCell>
+                            <TableCell className="text-left hidden md:table-cell">$250.00</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </div>
         </div>
     )
